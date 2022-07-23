53896dcaf5c4751c15c83b5a1270911ad4921beb
ferd <beowulf1416@gmail.com>
2022-07-23 22:25:57 +0800

    add material button

--- ui/src/health/src/material.module.ts
+++ ui/src/health/src/material.module.ts
@@ -1,5 +1,6 @@
 import {NgModule} from '@angular/core';
 
+import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatSidenavModule} from '@angular/material/sidenav';
@@ -10,6 +11,7 @@ import {MatBadgeModule} from '@angular/material/badge';
 
 @NgModule({
     exports: [
+        MatButtonModule,
         MatIconModule,
         MatToolbarModule,
         MatSidenavModule,
