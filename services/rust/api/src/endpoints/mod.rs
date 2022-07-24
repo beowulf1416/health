c9a45e3104b9eff3f028f36462bd53e4d647674e
ferd <beowulf1416@gmail.com>
2022-07-24 09:39:49 +0800

    add api server

---
+++ services/rust/api/src/endpoints/mod.rs
@@ -0,0 +1,14 @@
+use log::{
+    info
+};
+
+use actix_web::{
+    HttpResponse, 
+    Responder 
+};
+
+
+pub async fn status() -> impl Responder {
+    info!("status()");
+    return HttpResponse::Ok().body("Status");
+}
---
+++ services/rust/api/src/main.rs
@@ -0,0 +1,43 @@
+extern crate log;
+
+mod endpoints;
+
+use log::{ 
+    info 
+};
+
+use actix_web::{ 
+    HttpServer, 
+    App, 
+    web, 
+    HttpResponse, 
+    Responder 
+};
+
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::init();
+    info!("starting up");
+
+    let server = HttpServer::new(move || {
+        App::new()
+            // .configure(data::data::configure)
+            // .configure(users::jwt::configure)
+            // .configure(crate::utils::bus::configure)
+            // .wrap(crate::middleware::cache::Cache::new())
+            // .wrap(crate::middleware::cors::CORS::new())
+            // .wrap(crate::middleware::user::User::new())
+            // .service(web::scope("/status").configure(crate::endpoints::status::config))
+            // .service(web::scope("/user").configure(crate::endpoints::user::config))
+            // .service(web::scope("/tenants").configure(crate::endpoints::tenants::config))
+            // .service(web::scope("/admin/tenants").configure(crate::endpoints::tenant::admin::config))
+            .route("/status", web::get().to(crate::endpoints::status))
+    })
+    .bind("0.0.0.0:8081")?
+    .run();
+
+    info!("Server running at https://0.0.0.0:8081");
+    server.await
+}
