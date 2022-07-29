b0e45a283c89c16b09bb1d68f1c123edc722a139
ferd <beowulf1416@gmail.com>
2022-07-29 20:35:46 +0800

    additional material modules

--- ui/src/admin/src/material/material.module.ts
+++ ui/src/admin/src/material/material.module.ts
@@ -1,5 +1,7 @@
 import {NgModule} from '@angular/core';
 
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
@@ -11,6 +13,8 @@ import {MatBadgeModule} from '@angular/material/badge';
 
 @NgModule({
     exports: [
+        MatFormFieldModule,
+        MatInputModule,
         MatButtonModule,
         MatIconModule,
         MatToolbarModule,
