8a5de4aa55f192eb385828ae1184717d0b7e9a51
ferd <beowulf1416@gmail.com>
2022-07-29 20:36:04 +0800

    modify login form

--- ui/src/admin/src/app/modules/login/components/login/login.component.ts
+++ ui/src/admin/src/app/modules/login/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { TitleService } from 'src/app/services/title.service';
 
 @Component({
@@ -7,6 +8,16 @@ import { TitleService } from 'src/app/services/title.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+
+  loginForm = new FormGroup({
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email
+    ]),
+    pw: new FormControl('', [
+      Validators.required
+    ])
+  });
 
   constructor(
     private title: TitleService
@@ -17,4 +28,11 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  submit() {
+    console.log('LoginComponent::submit()');
+  }
 }
