d5b1ebc2ac5f6530c64b8c4d75e541b6fa4f9b40
ferd <beowulf1416@gmail.com>
2022-08-06 16:49:47 +0800

    add sp to set user active status

--- data/sql/postgresql/iam/procs.sql
+++ data/sql/postgresql/iam/procs.sql
@@ -2,6 +2,7 @@ set schema 'iam';
 
 \ir procs/users/user_add.sql
 \ir procs/users/user_set_password.sql
+\ir procs/users/user_set_active.sql
 \ir procs/users/user_authenticate.sql
 
 \ir procs/roles/role_add.sql
