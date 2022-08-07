120ffb74a0cc04cf59fe128340b7bd3d66270a83
ferd <beowulf1416@gmail.com>
2022-08-07 11:33:58 +0800

    add user procs

--- data/sql/postgresql/iam/procs.sql
+++ data/sql/postgresql/iam/procs.sql
@@ -4,5 +4,7 @@ set schema 'iam';
 \ir procs/users/user_set_password.sql
 \ir procs/users/user_set_active.sql
 \ir procs/users/user_authenticate.sql
+\ir procs/users/user_list.sql
+\ir procs/users/user_get.sql
 
 \ir procs/roles/role_add.sql
