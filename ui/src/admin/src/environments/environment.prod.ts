3871f532e88026bc1130f1c5769de256d0082538
ferd <beowulf1416@gmail.com>
2022-08-09 20:03:48 +0800

    add endpoints

--- ui/src/admin/src/environments/environment.prod.ts
+++ ui/src/admin/src/environments/environment.prod.ts
@@ -6,6 +6,21 @@ export const environment = {
   path_authenticate: '/user/authenticate',
 
   path_domain_add: '/domain/add',
+  path_domain_get: '/domain/get',
+  path_domain_list: '/domain/list',
+  path_domain_update: '/domain/update',
+  path_domain_set_active: '/domain/set/active',
+
   path_user_add: '/user/add',
-  path_role_add: '/role/add'
+  path_user_list: '/user/list',
+  path_user_get: '/user/get',
+  path_user_set_active: '/user/set/active',
+  path_user_set_password: '/user/set/password',
+  path_user_update: '/user/update',
+
+  path_role_add: '/role/add',
+  path_role_get: '/role/get',
+  path_role_list: '/role/list',
+  path_role_update: '/role/update',
+  path_role_set_active: '/role/set/active'
 };
