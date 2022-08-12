e2c33b0011769d8ed4cb3eae75c5e908255a5e31
ferd <beowulf1416@gmail.com>
2022-08-12 15:26:20 +0800

    add proc to retrieve user data using email address

--- data/sql/postgresql/iam/procs.sql
+++ data/sql/postgresql/iam/procs.sql
@@ -6,6 +6,7 @@ set schema 'iam';
 \ir procs/users/user_authenticate.sql
 \ir procs/users/user_list.sql
 \ir procs/users/user_get.sql
+\ir procs/users/user_get_by_email.sql
 
 \ir procs/roles/role_add.sql
 \ir procs/roles/role_list.sql
