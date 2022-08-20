39c9c96e25424b4336df9d953c2e8e368c4a718f
ferd <beowulf1416@gmail.com>
2022-08-21 06:53:27 +0800

    modify user service

--- ui/src/health/src/app/services/user.service.ts
+++ ui/src/health/src/app/services/user.service.ts
@@ -1,19 +1,109 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, filter, iif, map, Observable, of, Subject, switchMap, tap } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { ApiResponse, newApiResponse } from '../classes/api-response';
+import { User } from '../classes/user';
+
+
+// export class User {
+//   constructor(
+//     private email: string
+//   ) {}
+
+//   get is_authenticated(): boolean {
+//     return this.email != "";
+//   }
+// };
+
+
+// https://github.com/gothinkster/angular-realworld-example-app/blob/master/src/app/core/services/user.service.ts
+
+// export function JSONtoUser(
+//   json: JSON
+// ): User {
+//   const sz = JSON.stringify(json);
+//   const o = JSON.parse(sz);
+//   const u = o.user as User;
+//   return u;
+// }
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
+  user_subject = new BehaviorSubject<User>(new User(''));
+  user$ = this.user_subject.asObservable();
+
   constructor(
     private http: HttpClient
-  ) { }
+  ) { 
+    const token = sessionStorage.getItem(environment.key_session_token);
+    if (token != null) {
+      this._get_current_user().subscribe((r: ApiResponse) => {
+        console.log(r);
+      });
+    }
+  }
+
+  get current_user(): Observable<User> {
+    return this.user$;
+  }
+
+  _get_current_user(): Observable<ApiResponse> {
+    return this.http.post<ApiResponse>(
+      environment.url_base + environment.path_user_current,
+      {}
+    ).pipe(
+      filter((r: ApiResponse) => r.success === true),
+      tap((r: ApiResponse) => {
+        
+        const sz = JSON.stringify(r.data);
+        const o = JSON.parse(sz);
+        const u = o.user;
+
+        this.user_subject.next(new User(u?.email));
+      })
+    );
+  }
 
   login(
-    email: String,
-    password: String
-  ) {
+    email: string,
+    password: string
+  ): Observable<ApiResponse> {
+    return this.http.post<ApiResponse>(
+      environment.url_base + environment.path_authenticate,
+      {
+        email: email,
+        password: password
+      },
+      {
+        observe: 'response'
+      }
+    ).pipe(
+      tap(r => {
+        if (r.headers.has('authorization')) {
+          const header_value = r.headers.get('authorization');
+          if (header_value != null) {
+            const token = header_value.replace('Bearer ', '');
+            // console.log(token);
+            sessionStorage.setItem(environment.key_session_token, token);
+          }
+        }
+      }),
+      map((r: HttpResponse<ApiResponse>) => r.body),
+      switchMap((r: any) => {
+        if (r.success) {
+          return this._get_current_user();
+        } else {
+          return of(r);
+        }
+      })
+    );
+  }
 
+  logout() {
+    sessionStorage.removeItem(environment.key_session_token);
   }
 }
