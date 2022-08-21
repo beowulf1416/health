2c6e9a063dde8231440f6ce6f000a7f9f15dc5d6
ferd <beowulf1416@gmail.com>
2022-08-21 20:51:58 +0800

    add patient add endpoint

--- services/rust/api/src/endpoints/patient.rs
+++ services/rust/api/src/endpoints/patient.rs
@@ -30,6 +30,17 @@ use crate::endpoints::{
 };
 
 
+#[derive(Debug, Deserialize)]
+struct PatientAddRequest {
+    id: uuid::Uuid,
+    email: String,
+    given_name: String,
+    family_name: String,
+    prefix: String,
+    suffix: String
+}
+
+
 pub fn config(cfg: &mut web::ServiceConfig) {
     cfg
         .service(
@@ -39,4 +50,67 @@ pub fn config(cfg: &mut web::ServiceConfig) {
                 .route(web::post().to(patient_add_post))
         )
     ;
+}
+
+
+async fn patient_add_get() -> impl Responder {
+    info!("patient_add_get()");
+    return HttpResponse::Ok().body("use POST /add instead");
+}
+
+
+async fn patient_add_post(
+    _request: HttpRequest,
+    db: web::Data<Db>,
+    params: web::Json<PatientAddRequest>
+
+) -> impl Responder {
+    info!("patient_add_post()");
+
+    match db.pool().get().await {
+        Err(e) => {
+            error!("unable to retrieve client: {:?}", e);
+
+            return HttpResponse::InternalServerError()
+                .json(ApiResponse {
+                    success: false,
+                    message: String::from("// TODO patient add error"),
+                    data: None
+                });
+        }
+        Ok(client) => {
+            let id = params.id.clone();
+            let given_name = params.given_name.clone();
+            let family_name = params.family_name.clone();
+            let prefix = params.prefix.clone();
+            let suffix = params.suffix.clone();
+
+            let patients = Patients::new(client);
+            match patients.add(
+                &id,
+                &given_name,
+                &family_name,
+                &prefix,
+                &suffix
+            ).await {
+                Err(e) => {
+                    error!("unable to add patient record: {:?}", e);
+                    return HttpResponse::InternalServerError()
+                        .json(ApiResponse {
+                            success: false,
+                            message: String::from("// TODO patient add error"),
+                            data: None
+                        });
+                }
+                Ok(_) => {
+                    return HttpResponse::Ok()
+                        .json(ApiResponse {
+                            success: true,
+                            message: String::from("added domain record"),
+                            data: None
+                        });
+                }
+            }
+        }
+    }
 }
