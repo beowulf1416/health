051432c93dc1457da20f7b5b6e33d45ff3db6488
ferd <beowulf1416@gmail.com>
2022-08-23 22:10:24 +0800

    add fields to component

--- ui/src/health/src/app/components/patient/patient.component.ts
+++ ui/src/health/src/app/components/patient/patient.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 import { TitleService } from 'src/app/services/title.service';
 
 @Component({
@@ -9,7 +9,34 @@ import { TitleService } from 'src/app/services/title.service';
 })
 export class PatientComponent implements OnInit {
 
-  patientForm = new FormGroup({});
+  patientForm = new FormGroup({
+    family_name: new FormControl('', []),
+    given_name: new FormControl('', []),
+
+    contact_no: new FormControl('', []),
+    contact_address: new FormControl('', []),
+
+    gender: new FormControl('', []),
+    marital_status: new FormControl('', []),
+    ethnicity: new FormControl('', []),
+
+    admission_source: new FormControl('', []),
+    current_episode: new FormControl('', []),
+    
+    birth_date: new FormControl('', []),
+    admission_date: new FormControl('', []),
+    face_to_face_date: new FormControl('', []),
+    soc_date: new FormControl('', []),
+    certification_date: new FormControl('', []),
+    discharge_date: new FormControl('', []),
+
+    ss_id: new FormControl('', []),
+    medicare_id: new FormControl('', []),
+    medicaid_id: new FormControl('', []),
+    cbsa_id: new FormControl('', []),
+
+    release_of_info: new FormControl('', [])
+  });
 
   patient = {
     givenName: 'not set',
@@ -27,4 +54,59 @@ export class PatientComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  get family_name() {
+    return this.patientForm.get('family_name');
+  }
+
+  get given_name() {
+    return this.patientForm.get('given_name');
+  }
+
+  get address() {
+    return this.patientForm.get('contact_address');
+  }
+
+  get contact_no() {
+    return this.patientForm.get('contact_no');
+  }
+
+  get birth_date() {
+    return this.patientForm.get('birth_date');
+  }
+
+  get gender() {
+    return this.patientForm.get('gender');
+  }
+
+  get marital_status() {
+    return this.patientForm.get('marital_status');
+  }
+
+  get ethnicity() {
+    return this.patientForm.get('ethnicity');
+  }
+
+  get admission_source() {
+    return this.patientForm.get('admission_source');
+  }
+
+  get admission_date() {
+    return this.patientForm.get('admission_date');
+  }
+
+  get face_to_face_date() {
+    return this.patientForm.get('face_to_face_date');
+  }
+
+  get soc_date() {
+    return this.patientForm.get('soc_date');
+  }
+
+  get current_episode() {
+    return this.patientForm.get('current_episode');
+  }
+
+  get certification_date() {
+    return this.patientForm.get('certification_date');
+  }
 }
